4ddd7fa08a1ab5360880ae57eb0c3ff052c48f4c
kasp441 <91524039+kasp441@users.noreply.github.com>
2024-05-23 20:00:48 +0200

    forgotten changes, updated be

--- Domain/Book.cs
+++ Domain/Book.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
-    internal class Book
+    public class Book
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
     }
 }
