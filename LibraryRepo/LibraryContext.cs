293269dad42336f0f9b302bb66484938e40a7d63
kasp441 <91524039+kasp441@users.noreply.github.com>
2024-05-23 20:00:02 +0200

    base Be and db context, as well as start on onion

---
+++ LibraryRepo/LibraryContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryRepo
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Loans> Loans { get; set; }
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+        }
+    }
+}
