aeb3251e525bb39731d021c49c79ca083b39926f
kasp441 <91524039+kasp441@users.noreply.github.com>
2024-05-30 15:45:33 +0200

    added reservation with migrations and scripts

--- projects/LibraryRepo/Migrations/20240530131859_AddedReservations.Designer.cs
+++ projects/LibraryRepo/Migrations/20240530131859_AddedReservations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repo;
 
@@ -11,9 +12,11 @@ using Repo;
 namespace Repo.Migrations
 {
     [DbContext(typeof(LibraryContext))]
-    partial class LibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20240530131859_AddedReservations")]
+    partial class AddedReservations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
