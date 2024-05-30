f0f629f5db86df7d121a3f4f1cd3b9e942138e10
kasp441 <91524039+kasp441@users.noreply.github.com>
2024-05-30 16:44:53 +0200

    Update Program.cs

--- HealthScanner/Program.cs
+++ HealthScanner/Program.cs
@@ -17,20 +17,27 @@ namespace DatabaseHealthScanner
                 try
                 {
                     var appliedMigrations = context.Database.GetAppliedMigrations().ToList();
+                    Console.Clear();
                     Console.WriteLine("Database is up");
                     Console.WriteLine("Number of Applied Migrations: " + appliedMigrations.Count);
                     foreach (var migration in appliedMigrations)
                     {
                         Console.WriteLine(migration);
                     }
+                    if (secondsDown > 0)
+                    {
+                        Console.WriteLine($"Database was down for {secondsDown} seconds");
+                        secondsDown = 0;
+                    }
                 }
                 catch (Exception ex)
                 {
+                    Console.Clear();
                     secondsDown++;
                     Console.WriteLine("Error connecting to database: " + ex.Message);
                     Console.WriteLine($"Database has been down for {secondsDown} seconds");
                 }
-                Thread.Sleep(1000);
+                Thread.Sleep(100);
             }
         }
     }
