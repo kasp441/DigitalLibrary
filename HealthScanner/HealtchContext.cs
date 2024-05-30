7fd5ab475d59cbc15519846a188dd8567f7f7c11
kasp441 <91524039+kasp441@users.noreply.github.com>
2024-05-30 16:35:24 +0200

    added healthchecker to project of database

---
+++ HealthScanner/HealtchContext.cs
@@ -0,0 +1,18 @@
+﻿using DatabaseHealthScanner;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthScanner
+{
+    public class HealtchContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=104.238.191.68,1433;Database=deployment;User Id=sa;Password=SuperSecret7!;Trusted_Connection=False;TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ HealthScanner/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using HealthScanner;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseHealthScanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secondsDown = 0;
+            using var context = new HealtchContext();
+            while (true)
+            {
+                try
+                {
+                    var appliedMigrations = context.Database.GetAppliedMigrations().ToList();
+                    Console.WriteLine("Database is up");
+                    Console.WriteLine("Number of Applied Migrations: " + appliedMigrations.Count);
+                    foreach (var migration in appliedMigrations)
+                    {
+                        Console.WriteLine(migration);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    secondsDown++;
+                    Console.WriteLine("Error connecting to database: " + ex.Message);
+                    Console.WriteLine($"Database has been down for {secondsDown} seconds");
+                }
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
